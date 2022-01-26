cbcdf5bcbd803e94c29ebe315f68ed5053e9373b
svwolter <sven.wolter@iqb.hu-berlin.de>
2022-01-26 19:57:58 +0100

    Implement not-found page

--- apps/iqb-personnel-management/src/app/app.module.ts
+++ apps/iqb-personnel-management/src/app/app.module.ts
@@ -9,19 +9,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { PersonnelComponent } from './personnel/personnel.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthenticationInterceptor } from './authentication/authentication.interceptor';
 import { AuthenticationGuardService } from './authentication/authentication-guard.service';
 import { PersonnelService } from './services/personnel.service';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, PersonnelComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PersonnelComponent,
+    NotFoundComponent,
+  ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatButtonModule,
     FormsModule,
+    MatButtonModule,
   ],
   providers: [
     {
