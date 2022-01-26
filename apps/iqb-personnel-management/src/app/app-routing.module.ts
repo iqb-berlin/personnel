664a4b851d10d2a056664644d81e6b240e34e774
svwolter <sven.wolter@iqb.hu-berlin.de>
2022-01-26 20:00:41 +0100

    Implement basic routing

--- apps/iqb-personnel-management/src/app/app-routing.module.ts
+++ apps/iqb-personnel-management/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthenticationGuardService } from './authentication/authentication-guard.service';
+import { LoginComponent } from './authentication/login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PersonnelComponent } from './personnel/personnel.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/personnel', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'personnel',
+    component: PersonnelComponent,
+    canActivate: [AuthenticationGuardService],
+  },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
